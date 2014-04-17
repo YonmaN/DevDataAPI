b980b7b6064fc5e86bf5e505791381350b4273e7
yonni <yonni.m@zend.com>
2014-04-17 16:35:47 +0300

    Added Data class

---
+++ DevbarData.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Zend\Devbar;
+
+class Data {
+	/**
+	 * @var array
+	 */
+	static $registry = array();
+	/**
+	 * @var integer
+	 */
+	static $requestId = 0;
+	
+	/**
+	 * @param string $namespace
+	 * @param string $value
+	 */
+	static function set($namespace, $value) {
+		static::$registry[$namespace] = $value;
+	}
+	
+	/**
+	 * @param string $namespace
+	 */
+	static function get($namespace) {
+		return static::$registry[$namespace];
+	}
+	
+	/**
+	 * @param string $namespace
+	 */
+	static function remove($namespace) {
+		unset(static::$registry[$namespace]);
+	}
+	
+	static function saveClose() {
+		if (isset(static::$registry['__save_close_flag']) && static::$registry['__save_close_flag']) {
+			trigger_error('Devbar data already written', E_USER_WARNING);
+			return ;
+		}
+	
+		unset($zdbRegistry['__save_close_flag']);
+	
+		$varpath = get_cfg_var('zend.data_dir') . DIRECTORY_SEPARATOR . 'db';
+		$zdbPDO = new \PDO("sqlite:{$varpath}/devbar.db", '', '', array());
+	
+		$stmt = $zdbPDO->prepare('INSERT INTO devbar_custom_data VALUES(NULL, ?, ?, ?)');
+	
+		$zdbPDO->beginTransaction();
+		foreach(static::$registry as $namespace => &$value) {
+			$stmt->execute(array(intval(static::$requestId), $namespace, json_encode($value)));
+		}
+		$zdbPDO->commit();
+	
+		$zdbRegistry['__save_close_flag'] = true;
+	}
+	
+}
+register_shutdown_function('Zend\Devbar\Data::saveClose');
+
+
+Data::set('namepsace', [['boom' => 'value']]);
